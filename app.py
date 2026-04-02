bfd00dac3dd271b913131e4a6f637f7505559c53
Nishanth Pudota <nishanthpudota3860@gmail.com>
2026-04-02 10:48:24 +0000

    post deployment

--- app.py
+++ app.py
@@ -364,4 +364,4 @@ def delete_task(task_id):
 
 # ══════════════════════════════════════════════════════════════════════════════
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=5000)
